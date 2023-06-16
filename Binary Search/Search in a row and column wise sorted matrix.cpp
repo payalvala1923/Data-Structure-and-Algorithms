6b6509a88ed39b491f02050971bc8ae6b4638d98
Payal Vala <101017007+payalvala1923@users.noreply.github.com>
2023-06-16 18:30:36 +0530

    Update Search in a row and column wise sorted matrix.cpp

--- Binary Search/Search in a row and column wise sorted matrix.cpp
+++ Binary Search/Search in a row and column wise sorted matrix.cpp
@@ -9,3 +9,25 @@
         return false;
 
     }
+
+
+
+ bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       int lo = 0;
+        if(!n) return false;
+        int hi = (n* m) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
