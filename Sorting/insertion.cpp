2f007132fbf4dd20bd47231074252b294c727358
Payal Vala <101017007+payalvala1923@users.noreply.github.com>
2023-06-16 09:48:25 +0530

    Create insertion.cpp

---
+++ Sorting/insertion.cpp
@@ -0,0 +1,13 @@
+void insertionSort(int arr[], int n)
+    {
+        for(int i=1;i<n;++i){
+            int j=i-1;
+            int x=arr[i];
+            while(j>=0 && (arr[j]>x)){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=x;
+        }
+    }
+
