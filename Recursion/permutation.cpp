0bf22b1ef41b05231b94261d0cc0d4a618e97a62
Payal Vala <101017007+payalvala1923@users.noreply.github.com>
2023-07-01 19:50:47 +0530

    Create permutation.cpp

---
+++ Recursion/permutation.cpp
@@ -0,0 +1,17 @@
+void solve(vector<int>& nums, int index,  vector<vector<int>> & ans){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index; i<nums.size();++i){
+            swap(nums[index], nums[i]);
+            solve(nums, index+1, ans);
+            //backtract
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(nums,0, ans);
+        return ans;
+    }
